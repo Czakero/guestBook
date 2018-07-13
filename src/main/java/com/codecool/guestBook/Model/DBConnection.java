8e1eece4158ed60234c543c932796e9607b0025c
Czakero <bartosz.pyrz96@gmail.com>
2018-07-13 21:35:47 +0200

    Fix connect method

--- src/main/java/com/codecool/guestBook/Model/DBConnection.java
+++ src/main/java/com/codecool/guestBook/Model/DBConnection.java
@@ -7,12 +7,13 @@ public final class DBConnection implements DBConnectionInterface {
     private static Connection db = null;
 
     @Override
-    public void connect() throws SQLException {
+    public Connection connect() throws SQLException {
         if (db == null) {
             db = DriverManager.getConnection(url, login, password);
         } else if (db.isClosed()) {
             db = DriverManager.getConnection(url, login, password);
         }
+        return db;
     }
 
     @Override
@@ -20,12 +21,5 @@ public final class DBConnection implements DBConnectionInterface {
         if (db != null && !db.isClosed()) {
             db.close();
         }
-    }
-
-    @Override
-    public ResultSet executeQuery(String query) throws SQLException {
-        PreparedStatement stmt = db.prepareStatement(query);
-        ResultSet rs = stmt.executeQuery();
-        return rs;
     }
 }
